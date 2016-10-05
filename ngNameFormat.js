aea68638c727b0c6df22f90de69ebf979c112c48
João Carvalho <oi@joaocarvalhowd.com.br>
2016-10-05 16:03:58 -0300

    Directive Name Format

---
+++ ngNameFormat.js
@@ -0,0 +1,53 @@
+(function(angular) {
+
+	'use strict';
+
+	angular
+		.module('ngNameFormat', [])
+		.directive('ngNameFormat', ngNameFormat);
+
+	function ngNameFormat() {
+		return {
+			require: 'ngModel',
+			restrict: 'A',
+			link: function(scope, el, attrs, ngModel) {
+
+				el.bind('keyup', function(e) {
+
+					if(e.keyCode >= 65 && e.keyCode <= 90 && !e.altKey && !e.ctrlKey) {
+						var value = ngModel.$viewValue;
+
+						ngModel.$setViewValue(capitalizeFirstLetter(value));
+						ngModel.$render();
+			        }
+
+				})
+
+			}
+		}
+	}
+
+	function capitalizeFirstLetter(str) {
+	    str = str.replace(/\s\s+/, ' ');
+	    
+		return str
+				.toLowerCase()
+				.split(' ')
+				.map(function(word) {
+				var ignore = ['da', 'de', 'das', 'do', 'dos'];
+
+				if(word.length > 1 && ignore.indexOf(word) === -1) {
+
+					return word[0].toUpperCase() + word.substr(1);
+
+				} else {
+
+					return word;
+
+				}
+
+			})
+			.join(' ');
+	}
+
+})(window.angular);
